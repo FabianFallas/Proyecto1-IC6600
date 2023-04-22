d598e30fcb625d668610207b719c954aa91518e2
FabianFallas <37458918+FabianFallas@users.noreply.github.com>
2023-04-21 21:38:39 -0600

    Lectura de buffer y semaforos
    Se agrega la lectur adel buffer y no liea por linea del archivo, ademasSi hay más archivos que el número máximo de hilos permitido, se espera a que todos los hilos terminen antes de continuar. Sin embargo, esto no garantiza que todos los hilos hayan terminado antes de crear nuevos hilos, para solucionar esto se usa un semaforo

--- Proyecto1.c
+++ Proyecto1.c
@@ -1,13 +1,9 @@
-///////////////////////////////////////////////////////////
-//El codigo se ejecuta de la siguiente forma e  visual studio code:
-//- gcc -o Proyecto1 Proyecto1.c -lpthread
-//- ./Proyecto1 palabraabuscar archivo
-//////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <regex.h>
+#include <semaphore.h>
 
 #define MAX_THREADS 4 // número máximo de hilos
 #define BUFFER_SIZE 1024 // tamaño del buffer
@@ -16,6 +12,7 @@ typedef struct {
     FILE *file; // puntero al archivo que se está procesando
     char *buffer; // buffer para almacenar los datos leídos del archivo
     size_t size; // tamaño del buffer
+    size_t pos; // posición actual dentro del buffer
     regex_t regex; // expresión regular que se está buscando
 } thread_data_t;
 
@@ -24,15 +21,29 @@ void *worker(void *arg) {
     thread_data_t *data = (thread_data_t *)arg;
     char *line = NULL;
     size_t line_len = 0;
-    int offset = 0;
     int result;
     while (1) {
-        // lee una línea del archivo
-        if (getline(&line, &line_len, data->file) == -1) {
-            // si no se puede leer más del archivo, cierra el archivo y libera el buffer
-            fclose(data->file);
-            free(data->buffer);
+        // busca una línea dentro del buffer
+        char *nl = memchr(&data->buffer[data->pos], '\n', data->size - data->pos);
+        if (nl != NULL) {
+            // se encontró una línea, la copia a un nuevo buffer
+            line_len = nl - &data->buffer[data->pos] + 1;
+            line = malloc(line_len + 1);
+            memcpy(line, &data->buffer[data->pos], line_len);
+            line[line_len] = '\0';
+            data->pos += line_len;
+        } else if (feof(data->file)) {
+            // si se llegó al final del archivo, sale del ciclo
             break;
+        } else {
+            // si no se encontró una línea y no se llegó al final del archivo, lee más datos del archivo al buffer
+            memmove(data->buffer, &data->buffer[data->pos], data->size - data->pos);
+            data->size -= data->pos;
+            data->pos = 0;
+            size_t n = fread(&data->buffer[data->size], 1, BUFFER_SIZE - data->size, data->file);
+            data->size += n;
+            data->buffer[data->size] = '\0';
+            continue;
         }
         // realiza la búsqueda en la línea leída usando la expresión regular
         result = regexec(&data->regex, line, 0, NULL, 0);
@@ -44,23 +55,11 @@ void *worker(void *arg) {
             fprintf(stderr, "Error en expresión regular\n");
             exit(1);
         }
-        // actualiza el desplazamiento en el archivo
-        offset += strlen(line);
-        if (offset >= data->size) {
-            // si el desplazamiento es mayor o igual al tamaño del buffer, se lee más datos del archivo
-            offset = 0;
-            fseek(data->file, -1 * data->size, SEEK_CUR);
-            data->size = fread(data->buffer, 1, BUFFER_SIZE, data->file);
-            if (data->size == 0) {
-                // si no se pueden leer más datos del archivo, cierra el archivo y libera el buffer
-                fclose(data->file);
-                free(data->buffer);
-                break;
-            }
-        }
+        // libera la memoria asignada a la línea
+        free(line);
+        line = NULL;
+        line_len = 0;
     }
-    // libera la memoria asignada a la línea
-    free(line);
     return NULL;
 }
 
@@ -92,41 +91,38 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        // Leer el archivo en bloques hasta que se llegue al final
-        while (!feof(file)) {
-            // Si ya se han creado el número máximo de hilos permitidos, esperar a que terminen antes de continuar
-            if (num_threads == MAX_THREADS) {
-                for (int j = 0; j < MAX_THREADS; j++) {
-                    pthread_join(threads[j], NULL);
-                }
-                num_threads = 0;
+        // Asignar la estructura de datos correspondiente para el siguiente hilo
+        data[num_threads].file = file;
+        data[num_threads].buffer = malloc(BUFFER_SIZE + 1);
+        data[num_threads].size = fread(data[num_threads].buffer, 1, BUFFER_SIZE, file);
+        data[num_threads].buffer[data[num_threads].size] = '\0';
+        data[num_threads].pos = 0;
+        data[num_threads].regex = regex;
+
+        // Crear el siguiente hilo
+        pthread_create(&threads[num_threads], NULL, worker, &data[num_threads]);
+
+        // Aumentar el contador de hilos y verificar si se ha alcanzado el máximo de hilos
+        num_threads++;
+        if (num_threads == MAX_THREADS || i == argc - 1) {
+            // Esperar a que todos los hilos hayan terminado antes de continuar
+            for (int j = 0; j < num_threads; j++) {
+                pthread_join(threads[j], NULL);
             }
-
-            // Asignar la estructura de datos correspondiente para el siguiente hilo
-            data[num_threads].file = file;
-            data[num_threads].buffer = malloc(BUFFER_SIZE);
-            data[num_threads].size = fread(data[num_threads].buffer, 1, BUFFER_SIZE, file);
-            data[num_threads].regex = regex;
-
-            // Crear el siguiente hilo y aumentar el contador
-            pthread_create(&threads[num_threads], NULL, worker, &data[num_threads]);
-            num_threads++;
+            // Cerrar los archivos y liberar los buffers
+            for (int j = 0; j < num_threads; j++) {
+                fclose(data[j].file);
+                free(data[j].buffer);
+                data[j].buffer = NULL;
+            }
+            num_threads = 0;
         }
-    }
-
-    // Esperar a que terminen todos los hilos antes de liberar memoria
-    for (int i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
     }
 
     // Liberar la memoria utilizada por la expresión regular
     regfree(&regex);
 
-    // Liberar la memoria utilizada por las estructuras de datos de los hilos y cerrar los archivos
-    for (int i = 0; i < num_threads; i++) {
-        free(data[i].buffer);
-        fclose(data[i].file);
-    }
-
     return 0;
 }
+
+       
